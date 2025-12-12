57ddba63ec8c278957cecf6e8bb14bfbbdfc8822
Matthew Branning <48932625+juandap1@users.noreply.github.com>
2025-12-12 17:33:28 -0600

    type exit to quit

--- src/components/InputBoxComponent.tsx
+++ src/components/InputBoxComponent.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
-import { render, Box, Text } from "ink";
+import { useApp, Box, Text } from "ink";
 import TextInput from "ink-text-input";
 import type { OllamaCLI } from "../utils/ollamacli";
 
 const InputBoxComponent = ({ OllamaClient }: { OllamaClient: OllamaCLI }) => {
   const [value, setValue] = useState("");
+  const { exit } = useApp();
   //   const [submitted, setSubmitted] = useState("");
 
   return (
     <Box flexDirection="column" padding={1}>
       {/* {submitted && <Text color="green">You submitted: {submitted}</Text>} */}
 
-      <Text>Type something and press enter:</Text>
+      <Text>Type something and press enter or type "exit" to exit:</Text>
       <Box borderStyle="round" borderColor="white" paddingX={1} width={70}>
         <Text bold>&gt;</Text>
         <Box paddingLeft={1}>
@@ -19,6 +20,10 @@ const InputBoxComponent = ({ OllamaClient }: { OllamaClient: OllamaCLI }) => {
             value={value}
             onChange={setValue}
             onSubmit={(v) => {
+              if (v.trim().toLowerCase() === "exit") {
+                exit();
+                return;
+              }
               setValue("");
               OllamaClient.completionQuery(v);
             }}
